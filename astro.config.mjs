c42099b8e8ea744482a7ee9d3b94742d9e2c2f0b
FuseTim <fusetim.log@gmx.com>
2025-04-18 17:44:22 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite'
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [
+            Icons({
+                compiler: 'astro',
+            }),
+        ],
+    }
+});
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const HEADER_NAME = 'FuseTim';
+export const APP_NAME = 'FuseTim\'s Blog';
+export const APP_DESCRIPTION = 'Personal blog of a French Computer Science student, always ready to learn new things. A jack of all trades, I explore everything from robotics to decentralized and peer-to-peer systems.';
