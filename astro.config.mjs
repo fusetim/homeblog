2ed8de391cd246f5578134a9a802f246d6a38023
FuseTim <fusetim.log@gmx.com>
2025-10-11 19:34:46 +0200

    feat: enable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from 'astro/config';
 import Icons from 'unplugin-icons/vite'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: 'https://fusetim.me',
-    base: `/`,
-    vite: {
-        plugins: [
-            Icons({
-                compiler: 'astro',
-            }),
-        ],
-    }
+  output: 'static',
+  site: 'https://fusetim.me',
+  base: `/`,
+
+  vite: {
+      plugins: [
+          Icons({
+              compiler: 'astro',
+          }),
+      ],
+  },
+
+  integrations: [sitemap()]
 });
